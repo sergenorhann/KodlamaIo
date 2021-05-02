f5c366e13f5bc0468b2ccbd95cd3a5258e1be46c
sergenorhann <sergenorhann@gmail.com>
2021-05-03 01:21:19 +0300

    03.05.2021

---
+++ classes/src/classes/CustomerManager.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class CustomerManager {
+
+	public static void add() {
+		System.out.println("Eklendi");
+	}
+
+	public static void remove() {
+		System.out.println("Silindi");
+	}
+
+	public static void update() {
+		System.out.println("Güncellendi");
+	}
+}
